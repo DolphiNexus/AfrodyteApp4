9f49cdcbf5e1d3bf65f771fb92b3c96c2409a32d
DolpiNexus <jairo73456@gmail.com>
2019-02-23 22:19:23 -0800

    first commit

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,3 +1,6 @@
+import { HomePage } from './../home/home.page';
+
+import {  Platform, NavController } from '@ionic/angular';
 import { AngularFireAuth} from 'angularfire2/auth';
 import { User } from './../models/user';
 import { Component, OnInit } from '@angular/core';
@@ -14,14 +17,23 @@ export class LoginPage implements OnInit {
 
   user = {} as User; 
 
-  constructor( private router: Router, private fireAuth: AngularFireAuth ) { }
+  constructor( private router: Router, private navCtrl: NavController, private fireAuth: AngularFireAuth ) { }
 
   ngOnInit() {
   }
 
-  login() {
-
-  }
+    async login(user: User) {
+      try {
+      const info = await this.fireAuth.auth.signInWithEmailAndPassword(user.email, user.password);
+  
+      if(info) {
+        await this.navCtrl.navigateRoot('/home');  
+        }
+      }
+      catch(e) {
+        console.error(e);
+      }
+    }
 
   register() {
     this.router.navigateByUrl('/register');
